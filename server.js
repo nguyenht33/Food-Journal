c5db455531e0e0dcb9b917c48ab08a92db813d06
Hung Nguyen <nguyenht33@gmail.com>
2018-06-14 15:26:08 -0400

    fixing deployment

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const express = require('express'),
       app = express(),
       path = require('path');
@@ -34,27 +33,15 @@ app.use('/api/users/', usersRouter);
 app.use('/api/entries/', entriesRouter);
 app.use('/api/auth/', authRouter);
 
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-//   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-//   if (req.method === 'OPTIONS') {
-//     return res.send(204);
-//   }
-//   next();
-// });
-// const jwtAuth = passport.authenticate('jwt', { session: false });
-
 let server;
 
-function runServer(DATABASE_URL, port = PORT) {
+function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
-    mongoose.connect(DATABASE_URL, err => {
+    mongoose.connect(databaseUrl, err => {
       if (err) {
         return reject(err);
       }
-      server = app
-      	.listen(port, () => {
+      server = app.listen(port, () => {
 	        console.log(`Your app is listening on port ${port}`);
 	        resolve();
 	      })
